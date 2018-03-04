106965ad6394f73abf8d0d768eb88d972abafead
jfSoto <35599612+jfSoto@users.noreply.github.com>
2018-03-04 18:42:28 +0100

    Update PruebaED1.java

--- PruebaED/src/es/studium/PruebaED1/PruebaED1.java
+++ PruebaED/src/es/studium/PruebaED1/PruebaED1.java
@@ -5,4 +5,6 @@ public class PruebaED1
 	//Esto es una prueba para la Práctica de Entorno 
 	
 	//Esta es la segunda prueba para la Práctica de Entorno
+	
+	//Estas últimas líneas y este comentario han sido escritos por José Francisco Soto Camacho
 }
